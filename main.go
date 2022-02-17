cfd7a043489d8b6d37ab1562a20d16dfc81ef6f7
keensyo <k66mdg55@gmail.com>
2022-02-17 23:46:22 +0900

    execute text

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+func main() {
+	// scanner := bufio.NewScanner(os.Stdin)
+
+	// for scanner.Scan() {
+	// 	fmt.Println(scanner.Text())
+	// }
+
+	// if err := scanner.Err(); err != nil {
+	// 	fmt.Fprintln(os.Stderr, "読み込みに失敗しました:", err)
+	// }
+
+	// var N string
+	// fmt.Scan(&N)
+
+	// fmt.Println(strconv.Atoi("100"))
+
+	// fmt.Println(strconv.Itoa(100) + "円")
+
+	// fmt.Println(strconv.FormatInt(100, 16))
+
+	// fmt.Println(strconv.ParseBool("true"))
+
+	// fmt.Println(strings.Split("a b c", " "))
+
+	// fmt.Println(strings.Join([]string{"a", "b", "c"}, ","))
+
+	// fmt.Println(strings.Repeat("hoge", 2))
+
+	// fmt.Println(strings.HasPrefix("hoge_fuga", "hoge"))
+
+	// s1 := strings.Replace("郷に入っては郷に従え", "郷", "Go", 1)
+	// fmt.Println(s1)
+
+	// s2 := strings.Replace("郷に入っては郷に従え", "郷", "Go", -1)
+	// fmt.Println(s2)
+
+	// r := strings.NewReplacer("郷", "Go", "入れば", "入っては")
+
+	// s := r.Replace("郷に入れば郷に従え")
+	// fmt.Println(s)
+
+	// toUpper := func(r rune) rune {
+	// 	if 'a' <= r && r <= 'z' {
+	// 		return r - ('a' - 'A')
+	// 	}
+	// 	return r
+	// }
+
+	// s := strings.Map(toUpper, "Hello, World")
+	// fmt.Println(s)
+
+	// s := strings.Map(unicode.ToUpper, "Hello, World")
+
+	// fmt.Println(s)
+
+	// fmt.Println(strings.Map(unicode.ToLower, s))
+
+	// fmt.Println(strings.ToUpper("Hello, World"))
+
+	// fmt.Println(strings.ToLower("Hello, World"))
+}
